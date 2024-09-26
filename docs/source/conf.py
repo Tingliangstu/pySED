cb8d7b6302729bff6ec43c1496375bf1c1207d14
liangting <liangting.zj@gmail.com>
2024-09-26 13:22:39 +0800

    add tutorials

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -14,7 +14,7 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['recommonmark','sphinx_markdown_tables'] 
+extensions = ['recommonmark','sphinx_markdown_tables','nbsphinx'] 
 
 templates_path = ['_templates']
 exclude_patterns = []
--- docs/source/tutorials.rst
+++ docs/source/tutorials.rst
@@ -1,3 +1,11 @@
 Tutorials
 =========
 
+This section contains tutorials on using pySED.
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Jupyter Notebooks
+
+   notebook/SED_MoS2.ipynb
+   notebook/Test_num_qpoints.ipynb
